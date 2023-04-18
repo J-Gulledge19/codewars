a656b23f5dc6b36cf354755c67a01a42e75534b6
Josh Gulledge <jgulledge19@gmail.com>
2023-04-18 17:44:02 -0600

    Completed challenge 3

--- challenges.js
+++ challenges.js
@@ -69,3 +69,8 @@ function cakes(recipe, available) {
 
 // 5, 10, 2  -->  17
 
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// console.log(goals(5, 10, 2));
