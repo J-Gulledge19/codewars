8fb8c4ae6b492f1f0cf2745816908373c5c4f3d0
Josh Gulledge <jgulledge19@gmail.com>
2023-04-19 15:27:39 -0600

    Completed challenge 5

--- challenges.js
+++ challenges.js
@@ -106,3 +106,21 @@ websites.push("codewars");
 // Second quadrant, the quadrant in the top-left, contains all points with negative X, but positive Y
 // Third quadrant, the quadrant in the bottom-left, contains all points with negative X and Y
 // Fourth quadrant, the quadrant in the bottom-right, contains all points with positive X, but negative Y
+
+function quadrant(x, y) {
+    if ( x > 0 && y > 0 ) {
+        return 1;
+    } else if ( x < 0 && y > 0 ) {
+        return 2;
+    } else if ( x < 0 && y < 0 ) {
+        return 3;
+    } else if ( x > 0 && y < 0 ) {
+        return 4;
+    }
+}
+
+console.log(quadrant(1, 2));
+console.log(quadrant(3, 5));
+console.log(quadrant(-10, 100));
+console.log(quadrant(-1, -9));
+console.log(quadrant(19, -56));
