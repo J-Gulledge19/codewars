237a0d12f57514d2b8d394e31981608d4740110e
Josh Gulledge <jgulledge19@gmail.com>
2023-04-19 15:30:15 -0600

    Added challenge 6

--- challenges.js
+++ challenges.js
@@ -119,8 +119,25 @@ function quadrant(x, y) {
     }
 }
 
-console.log(quadrant(1, 2));
-console.log(quadrant(3, 5));
-console.log(quadrant(-10, 100));
-console.log(quadrant(-1, -9));
-console.log(quadrant(19, -56));
+// console.log(quadrant(1, 2));
+// console.log(quadrant(3, 5));
+// console.log(quadrant(-10, 100));
+// console.log(quadrant(-1, -9));
+// console.log(quadrant(19, -56));
+
+//---------------------Challenge 6---------------------
+
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+// function add(num) {
+//   return num + 1;
+// }
+
+// function mult(num) {
+//   return num * 30;
+// }
+
+// chain(2, [add, mult]);
+// returns 90;
