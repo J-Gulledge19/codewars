53d072453241393c62d8808a0e5b07f9f616a11a
Josh Gulledge <jgulledge19@gmail.com>
2023-04-21 15:49:49 -0600

    Finished challenge 7

--- challenges.js
+++ challenges.js
@@ -153,3 +153,9 @@ function chain(input, fs) {
 
 // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
+function removeChar(str){
+    let newStr = str.slice(1,-1);
+    return newStr;
+   };
+
+// console.log(removeChar('hello'));
