a695c84b505714afc6546e74410e01f836035bdd
Josh Gulledge <jgulledge19@gmail.com>
2023-05-03 16:37:58 -0600

    Solved challenge 9

--- challenges.js
+++ challenges.js
@@ -185,10 +185,17 @@ function otherAngle(a, b) {
 // makeNegative(-5);   // return -5
 // makeNegative(0);    // return 0
 // makeNegative(0.12); // return -0.12
+
 // Notes
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
 function makeNegative(num) {
-    // Code?
+    if ( num < 0 ) {
+        return num;
+    }
+    else {
+        negNum = num - (num * 2);
+        return negNum;
+    }
   }
