bcd9e357d0c5c45fced831687a2c437832feb84e
Josh Gulledge <jgulledge19@gmail.com>
2024-10-17 16:29:25 -0600

    Completed challenge 14

--- challenges.js
+++ challenges.js
@@ -276,4 +276,34 @@ function solution (roman) {
       return sum;
   }
 
-console.log(solution('XXXI'));
+// console.log(solution('XXXI'));
+
+//---------------------Challenge 14---------------------
+
+// A format for expressing an ordered list of integers is to use a comma separated list of either individual integers or a range of integers denoted by
+// the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints.
+// It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+// Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+// Example: solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+function solution(list){
+    let arr = [];
+    for ( let i = 0 ; i < list.length ; i++ ) {
+        let j = i;
+        while ( list[j] - list[j+1] === -1 ) {
+            j++;
+        }
+        if ( j === i ) {
+            arr.push(list[i].toString());
+        } else if ( j === i + 1 ) {
+            arr.push(list[i].toString(),list[j].toString());
+        } else {
+            arr.push(list[i].toString() + "-" + list[j].toString());
+        }
+        i = j;
+    }
+    return arr.join(",");
+}
+
+// console.log(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
