06981eb262626ad6836ef34cf405355eed879840
Josh Gulledge <jgulledge19@gmail.com>
2024-10-28 16:15:35 -0600

    Challenge 15 added

--- challenges.js
+++ challenges.js
@@ -307,3 +307,8 @@ function solution(list){
 }
 
 // console.log(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
+
+//---------------------Challenge 15---------------------
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
